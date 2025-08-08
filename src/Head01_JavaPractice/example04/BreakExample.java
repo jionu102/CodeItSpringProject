386750759c47b0e17fa220fc7174fc5b75b6b926
LeeJinWoo <jionu102@naver.com>
2025-08-08 15:33:28 +0900

    add BreakExample, ContinueExample

---
+++ src/Head01_JavaPractice/example04/BreakExample.java
@@ -0,0 +1,11 @@
+package Head01_JavaPractice.example04;
+
+public class BreakExample {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i == 5) break;
+
+            System.out.println(i);
+        }
+    }
+}
