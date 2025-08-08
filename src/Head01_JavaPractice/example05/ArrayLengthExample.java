979e5d234adaa340cceef5d6cee916c2da84a1b2
LeeJinWoo <jionu102@naver.com>
2025-08-08 17:15:46 +0900

    add ArrayLengthExample, ArrayToSort

---
+++ src/Head01_JavaPractice/example05/ArrayLengthExample.java
@@ -0,0 +1,9 @@
+package Head01_JavaPractice.example05;
+
+public class ArrayLengthExample {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+
+        System.out.println(nums.length);
+    }
+}
---
+++ src/Head01_JavaPractice/example05/ArrayToSort.java
@@ -0,0 +1,13 @@
+package Head01_JavaPractice.example05;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class ArrayToSort {
+    public static void main(String[] args) {
+        int[] nums = {5, 2, 8};
+
+        Arrays.sort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
