2216aeefc2c0217d9f2a8ff3399e39fd9d6f8bb7
LeeJinWoo <jionu102@naver.com>
2025-08-08 15:18:35 +0900

    add While and Do-While example

---
+++ src/Head01_JavaPractice/example04/DoWhileExample.java
@@ -0,0 +1,14 @@
+package Head01_JavaPractice.example04;
+
+public class DoWhileExample {
+    public static void main(String[] args) {
+        int i = 0;
+
+        do {
+            System.out.println("i = " + i);
+            i++;
+        } while (i < 5);
+
+        System.out.println(i);
+    }
+}
