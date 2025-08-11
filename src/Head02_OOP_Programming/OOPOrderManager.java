fed55dc7f64aa31205b394461df998b236f615eb
LeeJinWoo <jionu102@naver.com>
2025-08-11 13:02:53 +0900

    add OOPOrderManager

---
+++ src/Head02_OOP_Programming/OOPOrderManager.java
@@ -0,0 +1,30 @@
+package Head02_OOP_Programming;
+
+class Order {
+    String customer;
+    String product;
+    int quantity;
+    int price;
+
+    Order(String customer, String product, int quantity, int price) {
+        this.customer = customer;
+        this.product = product;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    int getTotalPrice() {
+        return quantity * price;
+    }
+
+    void printOrderSummary() {
+        System.out.println(this.customer + " 님이 주문한 " + this.product + " " + this.quantity + "권의 총액은 " + getTotalPrice() + "원입니다.");
+    }
+}
+
+public class OOPOrderManager {
+    public static void main(String[] args) {
+        Order order = new Order("Alice", "Book", 2, 15000);
+        order.printOrderSummary();
+    }
+}
