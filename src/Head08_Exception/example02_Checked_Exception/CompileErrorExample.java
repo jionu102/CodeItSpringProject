5a7784c03631657d9310f91cff8c943eedac9ed9
LeeJinWoo <jionu102@naver.com>
2025-08-13 19:01:18 +0900

    add ArrayVsArrayList

--- src/Head08_Exception/example02_Checked_Exception/CompileErrorExample.java
+++ src/Head08_Exception/example02_Checked_Exception/CompileErrorExample.java
@@ -1,9 +1,10 @@
 package Head08_Exception.example02_Checked_Exception;
 
 import java.io.FileReader;
+import java.io.IOException;
 
 public class CompileErrorExample {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         FileReader reader = new FileReader("test.txt"); // 컴파일 에러 발생
     }
 }
