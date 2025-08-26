72b4e4794fb50249658d73a9fcdb1604b6ef11fd
LeeJinWoo <jionu102@naver.com>
2025-08-26 13:26:16 +0900

    add ArrayList example

---
+++ src/Head14_Data_Structure/example01_ArrayList/ArrayListExample.java
@@ -0,0 +1,15 @@
+package Head14_Data_Structure.example01_ArrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<String>();
+        list.add("Apple");
+        list.add("Banana");
+        System.out.println(list.get(1));
+        list.remove(0);
+        System.out.println(list);
+    }
+}
